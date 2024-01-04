4649194108885b0b793e7f6ea003c61b90b750c3
Muhammad Fahad <muhammadfahadbinabid@gmail.com>
2024-01-04 14:21:48 +0500

    root commit

---
+++ NokriApp/NokriApp/Model/UserCredentials.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NokriApp.Model
+{
+    public class UserCredentials
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+       
+
+    }
+}
